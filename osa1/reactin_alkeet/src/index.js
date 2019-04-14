bae84c2879273da0ede25bedabef0a43946647e2
Jens Aitola <jjepaah@gmail.com>
2019-04-14 20:38:19 +0300

    Update 1.5

--- osa1/reactin_alkeet/src/index.js
+++ osa1/reactin_alkeet/src/index.js
@@ -2,25 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
-
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+            {
+                name: 'Reactin perusteet',
+                exercises: 10
+            },
+            {
+                name: 'Tiedonvälitys propseilla',
+                exercises: 7
+            },
+            {
+                name: 'Komponenttien tila',
+                exercises: 14
+            }
+        ]
+    }
+    
     return (
         <>
-            <Header course={course} />
-            <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-            <Total part1={exercises1} part2={exercises2} part3={exercises3} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </>
     )
 }
 
 const Header = (props) => (
-    <> 
+    <>
         <h1>{props.course}</h1>
     </>
 )
@@ -28,9 +38,9 @@ const Header = (props) => (
 const Content = (props) => {
     return(
         <>
-            <Part part={props.part1} exercises={props.exercises1} />
-            <Part part={props.part2} exercises={props.exercises2} />
-            <Part part={props.part3} exercises={props.exercises3} />
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
         </>
     )
 }
@@ -38,7 +48,7 @@ const Content = (props) => {
 const Part = (props) => {
     return(
         <>
-            {props.part} {props.exercises} <br />
+            {props.part.name} {props.part.exercises} <br />
         </>
     )
 }
@@ -46,7 +56,7 @@ const Part = (props) => {
 const Total = (props) => (
     <>
         <p>
-            yhteensä {props.part1 + props.part2 + props.part3} tehtävää
+            yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} tehtävää
         </p>
     </>
 )

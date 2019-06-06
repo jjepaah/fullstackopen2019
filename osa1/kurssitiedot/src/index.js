b3142bb845dab7d589cbb0a76d987e95a0c7b373
Jens Aitola <jjepaah@gmail.com>
2019-06-06 19:41:04 +0300

    index.js päivitys
    koodin järjestelyä

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -44,15 +44,15 @@ const Content = (props) => {
         </>
     )
 }
+const Part = (props) => {
+    return (
+        <p>{props.part.name} {props.part.exercises}</p>
+    )
+}
 const Total = (props) => {
     const sum = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
     return (
         <p>Number of exercises {sum}</p>
-    )
-}
-const Part = (props) => {
-    return (
-        <p>{props.part.name} {props.part.exercises}</p>
     )
 }
 

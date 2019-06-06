208d52ba3bd50f75758d012317fc3566ab45f99b
Jens Aitola <jjepaah@gmail.com>
2019-06-06 19:38:19 +0300

    Uusi index.js
    Tein koko kurssitiedot tehtävän alusta loppuun uudestaan koska tauon jälkeen vanha koodi oli aivan hirveää katsottavaa

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -2,41 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-            {
-                name: 'Reactin perusteet',
-                exercises: 10
-            },
-            {
-                name: 'Tiedonvälitys propseilla',
-                exercises: 7
-            },
-            {
-                name: 'Komponenttien tila',
-                exercises: 14
-            }
-        ]
-    }
-    
-    return (
-        <>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </>
-    )
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+    </div>
+  )
 }
 
-const Header = (props) => (
-    <>
+const Header = (props) => {
+    return (
         <h1>{props.course}</h1>
-    </>
-)
-
+    )
+}
 const Content = (props) => {
-    return(
+    return (
         <>
             <Part part={props.parts[0]} />
             <Part part={props.parts[1]} />
@@ -44,21 +44,16 @@ const Content = (props) => {
         </>
     )
 }
-
-const Part = (props) => {
-    return(
-        <>
-            <p>{props.part.name} {props.part.exercises}</p>
-        </>
+const Total = (props) => {
+    const sum = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+    return (
+        <p>Number of exercises {sum}</p>
     )
 }
-
-const Total = (props) => (
-    <>
-        <p>
-            yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} tehtävää
-        </p>
-    </>
-)
+const Part = (props) => {
+    return (
+        <p>{props.part.name} {props.part.exercises}</p>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
